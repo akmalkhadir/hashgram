14aafb5792d98197e139413ee09df6b1cd15375f
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-11-14 14:43:00 +0000

    activate create chat modal

--- js/api.js
+++ js/api.js
@@ -1,4 +1,7 @@
 const getUser = (userId) =>
   fetch(`http://hashgram-backend.herokuapp.com/api/v1/users/${userId}`)
     .then(resp => resp.json())
-    .catch(console.log('API Down'))
+
+const getChatroom =(roomId) =>
+  fetch(`http://hashgram-backend.herokuapp.com/api/v1/chatrooms/${roomId}`)
+    .then(resp => resp.json())
--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
 let userId = 1
 
 const chatRoomListEl = document.querySelector('.sidebar_chatroom_list')
+const createChatPopUp = document.querySelector('#createChatPopUp')
+const navbarButtons = document.querySelector('#mainNavbar')
+const navbarChatButton = navbarButtons.querySelector('.create_new_chat_btn')
+const chatWindow = document.querySelector('.chat_window')
+const chatWindowMessagesEl = document.querySelector('#chat_window_messages')
 
 let localData = {
   currentUser: {},
   chatrooms: [],
-  currentRoomId: 0
+  currentRoomId: 0,
+  currentRoom: {},
+  currentRoomMessages: []
 }
+
+navbarChatButton.addEventListener('click', (event) => {
+  console.log(event)
+  createChatPopUp.className += 'is-active'
+})
 
 //  Render Single Chatroom Name
 const renderChatRoomListItem = (chatroom) => {
@@ -18,8 +30,43 @@ const renderChatRoomListItem = (chatroom) => {
   chatRoomListEl.appendChild(chatRoomListItem)
 }
 
+// Render all Chatrooms Name
 const renderChatRoomListItems = chatrooms => {
   chatrooms.forEach(chatroom => renderChatRoomListItem(chatroom))
+}
+
+// Render one a message
+const renderMessage = (message) => {
+  let messageItem = document.createElement('article')
+  messageItem.className = 'chat_messages_item media'
+  messageItem.dataset.messageId = message.id
+  messageItem.innerHTML = `
+  <figure class="media-left">
+    <p class="image is-64x64">
+      <img src="https://avatars1.githubusercontent.com/u/1881633?s=88&v=4" class="user_avatar">
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p><strong class="user_name">Akmal Khadir</strong></p>
+      <p class="message_text">vdfjkbvjfdvbdfbvd vdksv dsc kn m5 43nkt hjf erkg rh t34. 543hjr 43 34nr j f4k 5h34r 3knr ;h23 r32nk 32;k rnk34;r 34n 4;3k 543nkr n34r3qnkr</p>
+    </div>
+  </div>
+  `
+  chatRoomListEl.appendChild(chatRoomListItem)
+}
+
+const clearChatWindow = () => {
+  chatWindow.innerHTML = `
+  <div class="box">
+  <h2>Please select or create a chatroom to start</h2>
+  <button class="button create_new_chat_btn">Create New</button>
+  </div>
+  `
+  chatWindow.querySelector('.create_new_chat_btn').addEventListener('click', (event) => {
+    console.log(event)
+    createChatPopUp.className += 'is-active'
+  })
 }
 
 getUser(userId)
@@ -27,4 +74,5 @@ getUser(userId)
     localData.currentUser = user
     localData.chatrooms = [...localData.currentUser.chatrooms]
     renderChatRoomListItems(localData.chatrooms)
+    clearChatWindow()
   })
