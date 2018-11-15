4290344955d89a449309ead7e69de22c30ce4cc0
ssong-eu <sangyoon.song@gmail.com>
2018-11-15 11:16:26 +0000

    test

--- js/encryption.js
+++ js/encryption.js
@@ -1,6 +1,6 @@
-// Import encryption modules
-var nacl = require(['../node_modules/tweetnacl/nacl'])
-nacl.util = require(['../node_modules/tweetnacl-util/nacl-util'])
+// // Import encryption modules
+// var nacl = require(['../node_modules/tweetnacl/nacl'])
+// nacl.util = require(['../node_modules/tweetnacl-util/nacl-util'])
 
 // Temporary setup for the basics
 const privateKey = document.querySelector("#private-key")
@@ -8,64 +8,66 @@ const public = document.querySelector("#public-key")
 const messageList = document.querySelector("#messages")
 const messageForm = document.querySelector("#message-form")
 const inputField = document.querySelector("#message-input")
-let keyPair
+let secretKey
 
-const newNonce = () => nacl.randomBytes(nacl.secretbox.nonceLength);
-const generateSignKey = () => nacl.sign.keyPair();
-const generateBoxKey = () => nacl.box.keyPair();
-
-
-const encryptMessage = (text, signPriv, boxPub) => {
-    let envelope = {
-        message: {nacl.sign(sign)}
-    }
-
-    const keyUint8Array = nacl.util.decodeBase64(publickey);
-
-    const nonce = newNonce();
-    const messageUint8 = nacl.util.decodeUTF8(message);
-    const box = nacl.secretbox(messageUint8, nonce, keyUint8Array);
-
-    const fullMessage = new Uint8Array(nonce.length + box.length);
-    fullMessage.set(nonce);
-    fullMessage.set(box, nonce.length);
-
-    const base64FullMessage = nacl.util.encodeBase64(fullMessage);
-    return base64FullMessage;
+// const newNonce = () => nacl.randomBytes(nacl.secretbox.nonceLength);
+// const generateSignKey = () => nacl.sign.keyPair();
+// const generateBoxKey = () => nacl.box.keyPair();
+const generateSignKey = () => {
+    fetch("https://us-central1-hashgram-222609.cloudfunctions.net/generateSignKey").then(res => res.json())
+    .then(res => secretKey = res.split(","))
 }
 
-const decryptMessage = () => {
+// const encryptMessage = (text, signPriv, boxPub) => {
+//     let envelope = {
+//         message: {nacl.sign(sign)}
+//     }
 
-}
+//     const keyUint8Array = nacl.util.decodeBase64(publickey);
 
-const createEntry = () => {
-    let el = document.createElement("li")
-    let button = document.createElement("button")
-    button.id = "decryptMessage"
-    el.appendChild(button)
-    return el
-}
+//     const nonce = newNonce();
+//     const messageUint8 = nacl.util.decodeUTF8(message);
+//     const box = nacl.secretbox(messageUint8, nonce, keyUint8Array);
 
-messageForm.addEventListener("submit", event => {
-    event.preventDefault()
-    let el = createEntry()
-    el.innerText = encryptMessage(inputField, keyPair.publicKey)
-    messageList.appendChild(el)
-    inputField.clear()
-})
+//     const fullMessage = new Uint8Array(nonce.length + box.length);
+//     fullMessage.set(nonce);
+//     fullMessage.set(box, nonce.length);
+
+//     const base64FullMessage = nacl.util.encodeBase64(fullMessage);
+//     return base64FullMessage;
+// }
+
+// const decryptMessage = () => {
+
+// }
+
+// const createEntry = () => {
+//     let el = document.createElement("li")
+//     let button = document.createElement("button")
+//     button.id = "decryptMessage"
+//     el.appendChild(button)
+//     return el
+// }
+
+// messageForm.addEventListener("submit", event => {
+//     event.preventDefault()
+//     let el = createEntry()
+//     el.innerText = encryptMessage(inputField, keyPair.publicKey)
+//     messageList.appendChild(el)
+//     inputField.clear()
+// })
 
 document.addEventListener("click", event => {
     if (event.target.id === "generateKeys"){
         let signPair = generateSignKey()
-        let boxPair = generateBoxKey()
+        // let boxPair = generateBoxKey()
         privateKey.innerHTML = `
-            SignKey Private: ${signPair.secretKey}
-            BoxKey Private: ${boxPair.secretKey}
+            SignKey Private: ${secretKey}
         `
-        public.innerHTML = `
-            SignKey Public: ${signPair.publicKey}
-            BoxKey Public: ${boxPair.publicKey}
-        `
+        // public.innerHTML = `
+        //     SignKey Public: ${signPair.publicKey}
+        //     BoxKey Public: ${boxPair.publicKey}
+        
     } else if (event.target.id === "decryptMessage") {
 
     }
--- js/singleKeyEncryption.js
+++ js/singleKeyEncryption.js
@@ -1,34 +1,39 @@
 // Import encryption modules
-var nacl = require(['../node_modules/tweetnacl/nacl'])
-nacl.util = require(['../node_modules/tweetnacl-util/nacl-util'])
+// var nacl = require(['../node_modules/tweetnacl/nacl'])
+// nacl.util = require(['../node_modules/tweetnacl-util/nacl-util'])
 let secretKey
-const newNonce = () => nacl.randomBytes(nacl.secretbox.nonceLength)
+// const newNonce = () => nacl.randomBytes(nacl.secretbox.nonceLength)
+const generateKey = () => {
+    fetch("https://us-central1-hashgram-222609.cloudfunctions.net/generateSignKey").then(res => res.json())
+    .then(res => secretKey = res.split(","))
+}
+
 
 // Generate random key
-const generateKey = () => {secretKey = nacl.util.encodeBase64(nacl.randomBytes(nacl.secretbox.keyLength))}
+// const generateKey = () => {secretKey = nacl.util.encodeBase64(nacl.randomBytes(nacl.secretbox.keyLength))}
 
 // Generate key based on userinput, MUST be exactly 32 char
 // const generateKey = (input) => {secretKey = nacl.util.encodeBase64(nacl.util.decodeUTF8(input))}
 
-const encrypt = (message, key) => {
-    let utf8message = nacl.util.decodeUTF8(message)
-    let utf8key = nacl.util.decodeBase64(key)
-    let nonce = newNonce()
-    let box = nacl.secretbox(utf8message, nonce, utf8key)
+// const encrypt = (message, key) => {
+//     let utf8message = nacl.util.decodeUTF8(message)
+//     let utf8key = nacl.util.decodeBase64(key)
+//     let nonce = newNonce()
+//     let box = nacl.secretbox(utf8message, nonce, utf8key)
 
-    let fullMessage = new Uint8Array(nonce.length + box.length)
-    fullMessage.set(nonce)
-    fullMessage.set(box, nonce.length)
-    return nacl.util.encodeBase64(fullMessage)
-}
+//     let fullMessage = new Uint8Array(nonce.length + box.length)
+//     fullMessage.set(nonce)
+//     fullMessage.set(box, nonce.length)
+//     return nacl.util.encodeBase64(fullMessage)
+// }
 
-const decrypt = (ciphertext, key) => {
-    let utf8key = nacl.util.decodeBase64(key)
-    let utf8message = nacl.util.decodeBase64(ciphertext)
-    let nonce = utf8message.slice(0, nacl.secretbox.nonceLength)
-    let message = utf8message.slice(nacl.secretbox.nonceLength, ciphertext.length)
+// const decrypt = (ciphertext, key) => {
+//     let utf8key = nacl.util.decodeBase64(key)
+//     let utf8message = nacl.util.decodeBase64(ciphertext)
+//     let nonce = utf8message.slice(0, nacl.secretbox.nonceLength)
+//     let message = utf8message.slice(nacl.secretbox.nonceLength, ciphertext.length)
 
-    let decrypted = nacl.secretbox.open(message, nonce, utf8key)
+//     let decrypted = nacl.secretbox.open(message, nonce, utf8key)
 
-    return nacl.util.encodeUTF8(decrypted)
-}
+//     return nacl.util.encodeUTF8(decrypted)
+// }
